158c81302944ac8b380e23a23f3240b228454445
Sam Fawks <samfawk@limabean.xyz>
2025-07-03 23:50:37 -0400

    update gitignore to not track wizCrypt -should be built locally

--- WizCrypt/src/main/java/io/beanchain/devsuite/runWizCryptHelp.java
+++
@@ -1,9 +0,0 @@
-package io.beanchain.devsuite;
-
-import io.beanchain.devsuite.WizHelperLite;
-
-public class runWizCryptHelp {
-    public static void main(String[] args) {
-        WizHelperLite.startCLI();
-    }
-}
