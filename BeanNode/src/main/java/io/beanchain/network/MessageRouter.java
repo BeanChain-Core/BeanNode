56bc754bef1d90a68f1b09d9b612b36f77a486b1
Non Fasho <samfawk@limabean.xyz>
2025-07-09 01:21:30 -0400

    cleanup

--- BeanNode/src/main/java/io/beanchain/network/MessageRouter.java
+++ BeanNode/src/main/java/io/beanchain/network/MessageRouter.java
@@ -8,8 +8,6 @@ import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.concurrent.ConcurrentHashMap;
-
 import org.iq80.leveldb.DBIterator;
 
 import com.beanpack.Block.Block;
